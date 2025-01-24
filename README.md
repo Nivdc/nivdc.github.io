f74a2eecc2738d0c0136978c98b8343400521ef6
Nivdc <nivdc@live.com>
2025-01-24 15:38:08 +0800

    update

--- README.md
+++ README.md
@@ -2,24 +2,21 @@
 这是我的github.io博客仓库，我在这里介绍它的实现方式。  
 如果你还没看过它的话，你可以点击->[我的博客](http://nivdc.github.io/)<-访问。
   
-其主要灵感来源于我看过的很多简洁的博客，和Apache的IndexOf页。  
+其主要灵感来源于我看过的很多简洁的博客(特别是[Blog of Ziyao](https://ziyao233.github.io/))，和Apache的IndexOf页。  
 我发现我并不需要很复杂的设计，只需要简单的markdown转html就能够实现不错的效果。  
 **注意：** 如果你要复用我的博客代码，请不要使用fork，不然仓库的历史记录中仍会保留我的博客内容。
   
 编写博客并不困难，难的是写完了以后还要进行一系列繁杂的部署流程才能更新到网站上。  
-我的解决办法是制作一个特殊的主页分发markdown渲染库和md文件，然后直接使用浏览器渲染md文件。  
-这个方法的几个小缺点在于:
-1. 由于没有构建过程，哪怕是加载一个极其简单的目录页也要下载大约128kb的依赖库文件。  
-2. 由于路由功能是在前端实现的，当访问文章的时候刷新页面的时候会导致404。应用到gituhb pages上面的时候，
-3. 由于单页面应用本身的特性，搜索引擎无法抓取完整的内容。由于这个原因我决定转换技术方向。
+我的解决办法是编写一个[git hooks](https://git-scm.com/book/zh/v2/%E8%87%AA%E5%AE%9A%E4%B9%89-Git-Git-%E9%92%A9%E5%AD%90)
+脚本，在提交前生成文件并随附提交。  
+git hooks本身支持多种脚本语言，而我希望能够尽可能地开箱即用，因此就需要使用一种普遍流行的脚本语言。  
+那么，还能有什么脚本语言能比python更流行呢？  
 
-在可预见的未来，也许我们最终还是要引入一个构建过程来解决文章分类的问题。  
-一个解决的思路是使用[git hooks](https://git-scm.com/book/zh/v2/%E8%87%AA%E5%AE%9A%E4%B9%89-Git-Git-%E9%92%A9%E5%AD%90)
-在提交前生成文件并随附提交。  
-不过这个问题我们最好留到文章多到需要分类的时候在解决。  
+另外，注意github pages本身也有一个[自带的静态站点生成器Jekyll](https://docs.github.com/zh/pages/setting-up-a-github-pages-site-with-jekyll/about-github-pages-and-jekyll)可以实现类似的功能，
+你只要在仓库里上传自己的md文件Jekyll就会自动为你生成html。
 
 #### 使用技术
-* [Marked](https://marked.js.org/) - markdown渲染库
+* [python-markdown2](https://github.com/trentm/python-markdown2) - markdown渲染库
 * [Alpinejs](https://alpinejs.dev/) - modern jQuery
 * [giscus](https://giscus.app/zh-CN) - 利用 GitHub Discussions 实现的评论系统 (comming soon)
 * CDN服务提供商 - [ZSTATIC](https://zstatic.net/)
--- index.js
+++ index.js
@@ -4,35 +4,15 @@
 const hostname = window.location.hostname
 const repo = hostname
 const owner = hostname.split('.')[0]
-// const repo = 'lobby'
-// const owner = 'nivdc'
 
 document.addEventListener('alpine:init', () => {
     Alpine.data('body', () => {
         const body = {
             async init(){
                 this.nav.clickButton({innerText:"文章列表"})
-
-                window.addEventListener("popstate", async (event) => {
-                    if(window.location.pathname === "/") 
-                        this.article.html = ""
-                    else {
-                        const md = await (await fetch(event.state.download_url)).text()
-                        this.article.html = marked.parse(md)
-                    }
-                })
-            },
-
-            header:{
-                get show(){
-                    return body.$data.article.html.length > 0 === false
-                }
             },
 
             nav:{
-                get show(){
-                    return body.$data.article.html.length > 0 === false
-                },
                 info:"",
                 files:[],
                 async clickButton(element){
@@ -40,7 +20,6 @@ document.addEventListener('alpine:init', () => {
                     switch(element.innerText){
                         case '文章列表':{
                             var response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/public/articles`)
-                            // var response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/`)
                         break}
 
                         case '草稿列表':{
@@ -49,7 +28,8 @@ document.addEventListener('alpine:init', () => {
                     }
 
                     if(response && response.ok){
-                        this.files = await response.json()
+                        this.files = (await response.json()).filter(fileObject => fileObject.name.endsWith('.html'))
+                        this.files.forEach(fileObject => fileObject.href = `${hostname} / ${fileObject.html_url.split('contents')[1]}`)
                     }else{
                         this.files = []
                     }
@@ -57,28 +37,10 @@ document.addEventListener('alpine:init', () => {
 
                     if(this.files.length === 0) this.info = "如果你看见这个，说明在这个列表下我还什么都没有写。";
                 },
-                async clickFile(fileObject){
-                    if(fileObject.name.endsWith('.md')){
-                        const md = await (await fetch(fileObject.download_url)).text()
-                        this.article.html = marked.parse(md)
-                        history.pushState(cloneDeep(fileObject), "", `./${fileObject.path.split('.')[0]}`)
-                    }
-                }
             },
-
-            article:{
-                html:"",
-                get show(){
-                    return this.html.length > 0
-                }
-            }
         }
 
         return body
     })
 
 })
-
-function cloneDeep(o){
-    return JSON.parse(JSON.stringify(o))
-}
